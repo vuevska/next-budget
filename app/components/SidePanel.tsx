e31e9f48241532fcb85ca38771c776634d0a8791
Maja Vuevska <maja.vuevska@gmail.com>
2026-01-27 15:59:26 +0100

    Add a simple Side Panel

---
+++ app/components/SidePanel.tsx
@@ -0,0 +1,7 @@
+export default function SidePanel() {
+  return (
+    <aside className="w-64 min-h-screen bg-slate-800">
+      <div className="m-2 p-5 text-white">Side Panel</div>
+    </aside>
+  );
+}
