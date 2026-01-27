3a23d34a1e61087a71df945f72ec8b40655fd2c9
Maja Vuevska <maja.vuevska@gmail.com>
2026-01-27 17:02:46 +0100

    Add Prisma ORM and create the Account Model

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const issue = await prisma.account.findMany();
+  console.log(issue);
+
   return (
     <main>
       <div className="m-2">Main Panel</div>
