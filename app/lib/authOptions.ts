deb3589f4977caf203c3d926d245456036b9d1f7
Maja Vuevska <maja.vuevska@gmail.com>
2026-02-19 12:40:00 +0100

    Prompt user to select an account after signing out

--- app/lib/authOptions.ts
+++ app/lib/authOptions.ts
@@ -9,6 +9,11 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
     }),
   ],
   session: {
