7d280b92656398634927f6bfe190a55c61b46e9e
Maja Vuevska <maja.vuevska@gmail.com>
2026-02-19 09:31:52 +0100

    Fix redirect link after sign out

--- app/components/signout/SignOutButton.tsx
+++ app/components/signout/SignOutButton.tsx
@@ -7,7 +7,7 @@ import { FiLogOut } from "react-icons/fi";
 const SignOutButton = () => {
   return (
     <Button
-      onClick={() => signOut({ redirect: true, callbackUrl: "/login" })}
+      onClick={() => signOut({ redirect: true, callbackUrl: "/auth/signin" })}
       className="w-full flex items-center justify-center gap-2 px-4 py-2 bg-slate-700 hover:bg-slate-600 transition-colors duration-200 text-white rounded-lg text-sm font-medium"
     >
       <FiLogOut size={16} />
