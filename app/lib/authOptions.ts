8c8a0e5ed9c3fd5561e74f6a57acaf1160d3e89f
Maja Vuevska <maja.vuevska@gmail.com>
2026-02-25 16:21:47 +0100

    Change prod db connection string

--- app/lib/authOptions.ts
+++ app/lib/authOptions.ts
@@ -8,6 +8,7 @@ import { signInSchema } from "./validationSchema";
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
